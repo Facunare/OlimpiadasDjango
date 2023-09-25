81d16b5e1a9c4de17e29bf36b666e1d07c9ff6c3
facunare <arechaga.facundoet36@gmail.com>
2023-09-25 10:31:17 -0300

    cosillas

--- OlimpiadasDjango/administracion/models.py
+++ OlimpiadasDjango/administracion/models.py
@@ -31,25 +31,14 @@ class Paciente(models.Model):
     fecha_nac_paciente = models.DateField()
     medico_asignado = models.ForeignKey(Medico, null=True, on_delete=models.CASCADE)
     zona = models.ForeignKey(Zona, default=None, on_delete=models.CASCADE)
-    domicilio_paciente = models.CharField(max_length=100, default="")
+    # domicilio_paciente = models.CharField(max_length=100, default="")
+    # genero_paciente = models.CharField(max_length=1, default="")
+    # telefono_paciente = models.IntegerField(default="")
+    # email_paciente = models.CharField(max_length=100, default="")
+    # grupo_sanguineo = models.CharField(max_length=5, default="")
+    # factor_rh = models.BooleanField(default=False)
+    # peso_paciente_kg = models.IntegerField(default=0)
+    # altura_paciente = models.IntegerField(default=0)
+    # alergias = models.TextField(default="")
+    # consulta = models.TextField(default="")
 
-    genero_paciente = models.CharField(max_length=1, default="")
-    telefono_paciente = models.IntegerField(default="")
-    email_paciente = models.CharField(max_length=100, default="")
-    grupo_sanguineo = models.CharField(max_length=5, default="")
-    factor_rh = models.BooleanField(default=False)
-    peso_paciente_kg = models.IntegerField(default=0)
-    altura_paciente = models.IntegerField(default=0)
-    alergias = models.TextField(default="")
-    consulta = models.TextField(default="")
-
-
-# Editar informacion de paciente
-# Asignar formas de llamado
-# Crear tabla Profile, para que al crearse un usuario comun, se cree un perfil. Y solo un admin puede agregar pacientes si este tiene perfil. Y desde ese perfil, puede activar llamado.
-# Aumentar datos pacientes
-# Usuario generico
-# Calcular tiempo de respuesta promedio
-# Visualizar tablas y graficos
-# Filtrar reportes por area, origen del llamado (cama o baño), fecha y hora
-# Diseño
