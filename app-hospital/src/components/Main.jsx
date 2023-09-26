054f54eeaa8a8ca32c736fe573dbd1cc5d0709f7
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-09-26 09:42:46 -0300

    App-hospital

---
+++ app-hospital/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Constants from "expo-constants"
+
+
+const Main = () => {
+    return (
+        <View style={{marginTop: Constants.statusBarHeight}} >
+            <Text>Rate repository aplication</Text>
+        </View>
+    )
+}
+
+export default Main
